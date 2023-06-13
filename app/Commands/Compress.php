0bda997c91d97df9868787f476898128b8608a9e
Gamdara <poiu12345654321@gmail.com>
2023-06-13 22:15:47 +0700

    compress command add pagination

--- app/Commands/Compress.php
+++ app/Commands/Compress.php
@@ -17,46 +17,52 @@ class Compress extends BaseCommand
     {
         CLI::write('Begin compressing.......');
         $model = new DataDiriModel();
-        $datadiri = $model->findAll();
+        $offset = 0;
         
-        foreach($datadiri as $d){
-            CLI::write('ID:'.$d['id'], 'yellow');
-            CLI::write('......compressing', 'white');
+        do{
+            CLI::write('PAGE:'.($offset/10), 'yellow');
+            $datadiri = $model->findAll(10,$offset);
+            $offset += 10;
             
-            try{
-                $temp_source_image = tempnam(sys_get_temp_dir(), 'image');
+            foreach($datadiri as $d){
+                CLI::write('ID:'.$d['id'], 'yellow');
+                CLI::write('......compressing', 'white');
                 
-                $blob = $d['ktp'];
-                file_put_contents($temp_source_image, base64_decode($blob));
+                try{
+                    $temp_source_image = tempnam(sys_get_temp_dir(), 'image');
+                    
+                    $blob = $d['ktp'];
+                    file_put_contents($temp_source_image, base64_decode($blob));
+        
+                    $handler = service('image')->withFile($temp_source_image);
+                    $handler->resize(intval($handler->getWidth()/4),intval($handler->getHeight()/4), true);
+                    $handler->save();
+                    
+                    $d['ktp'] = base64_encode(file_get_contents($temp_source_image));
+        
+                    $blob = $d['ktp_selfie'];
+                    file_put_contents($temp_source_image, base64_decode($blob));
+        
+                    $handler = service('image')->withFile($temp_source_image);
+                    $handler->resize(intval($handler->getWidth()/4),intval($handler->getHeight()/4), true);
+                    $handler->save();
+                    
+                    $d['ktp_selfie'] = base64_encode(file_get_contents($temp_source_image));
+                    
+                    unlink($temp_source_image);
     
-                $handler = service('image')->withFile($temp_source_image);
-                $handler->resize($handler->getWidth()/4,$handler->getHeight()/4, true);
-                $handler->save();
-                
-                $d['ktp'] = base64_encode(file_get_contents($temp_source_image));
+                    CLI::write('......updating', 'white');
+                    
+                    $model->update($d['id'],$d);
     
-                $blob = $d['ktp_selfie'];
-                file_put_contents($temp_source_image, base64_decode($blob));
-    
-                $handler = service('image')->withFile($temp_source_image);
-                $handler->resize($handler->getWidth()/4,$handler->getHeight()/4, true);
-                $handler->save();
+                    CLI::write('......OK', 'green');   
                 
-                $d['ktp_selfie'] = base64_encode(file_get_contents($temp_source_image));
-                
-                unlink($temp_source_image);
-
-                CLI::write('......updating', 'white');
-                
-                $model->update($d['id'],$d);
-
-                CLI::write('......OK', 'green');   
-            
+                }
+                catch(\Error $e){
+                    CLI::write('......FAILED', 'red');               
+                }
             }
-            catch(\Error $e){
-                CLI::write('......FAILED', 'red');               
-            }
-        }
+        }while($datadiri !== []);
 
         CLI::write('COMPLETE','green');
     }
